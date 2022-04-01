07eb04432f91cbedb56f834c82fd00b29ca52012
Abhi Krishna A <im.abhikrishna@gmail.com>
2022-04-01 23:52:47 +0530

    2

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def welcomeScreen():
             #changing birdno
             if event.type==KEYDOWN and event.key==K_RIGHT:
                 birdno=(birdno+1)%3             
-            if event.type==KEYDOWN and event.key==K_RIGHT:
+            if event.type==KEYDOWN and event.key==K_LEFT:
                 birdno=(birdno-1)%3
             # If the user presses space or up key, start the game for them
             elif event.type==KEYDOWN and (event.key==K_SPACE or event.key == K_UP):
@@ -47,7 +47,29 @@ def welcomeScreen():
                 SCREEN.blit(GAME_SPRITES['base'], (basex, GROUNDY))    
                 pygame.display.update()
                 FPSCLOCK.tick(FPS)
-
+def gameover():
+    global birdno
+    playerx = int(SCREENWIDTH/5)
+    playery = int((SCREENHEIGHT - GAME_SPRITES['player'][birdno].get_height())/2)
+    messagex = int((SCREENWIDTH - GAME_SPRITES['message'].get_width())/2)
+    messagey = int(SCREENHEIGHT*0.13)
+    basex = 0
+    while True:
+        for event in pygame.event.get():
+            # if user clicks on cross button, close the game
+            if event.type == QUIT or (event.type==KEYDOWN and event.key == K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+            # If the user presses space or up key, start the game for them
+            elif event.type==KEYDOWN and (event.key==K_SPACE or event.key == K_UP):
+                return
+            else:
+                SCREEN.blit(GAME_SPRITES['background'], (0, 0))    
+               # SCREEN.blit(GAME_SPRITES['player'][birdno], (playerx, playery))    
+                SCREEN.blit(GAME_SPRITES['message'], (messagex,messagey ))    
+                SCREEN.blit(GAME_SPRITES['base'], (basex, GROUNDY))    
+                pygame.display.update()
+                FPSCLOCK.tick(FPS)
 def mainGame():
     score = 0
     playerx = int(SCREENWIDTH/5)
@@ -225,3 +247,4 @@ if __name__ == "__main__":
     while True:
         welcomeScreen() # Shows welcome screen to the user until he presses a button
         mainGame() # This is the main game function
+        gameover() 
